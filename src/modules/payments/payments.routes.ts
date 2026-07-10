9fd0d485cb9ed5c9827a608f1fd61b80ce4c5f7e
Shahiduz Zaman Munim <sz.munim@gmail.com>
2026-07-11 00:24:16 +0600

    Added payments routes

---
+++ src/modules/payments/payments.routes.ts
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import { Role } from "../../../generated/prisma/enums";
+import { auth } from "../../middlewares/auth";
+import { paymentsController } from "./payments.controller";
+
+const router = Router();
+
+router.post(
+    "/create/:id",
+    auth(Role.CUSTOMER),
+    paymentsController.createPayment,
+);
+
+router.post("/verify/:status", paymentsController.verifyPayment);
+
+router.get("/check/:tranId", paymentsController.checkPayment);
+
+export const paymentsRoutes = router;
